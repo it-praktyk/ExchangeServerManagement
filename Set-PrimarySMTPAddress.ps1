d797fefc5027e13a57a5ca4d44351f5a3fe485bd
Wojciech Sciesinski <wojciech@sciesinski.net>
2015-03-15 01:57:19 +0100

    0.3.0 - 2015-03-15 - Mode set extended, Operation parameter added

--- Set-PrimarySMTPAddress.ps1
+++ Set-PrimarySMTPAddress.ps1
@@ -13,11 +13,26 @@ Function Set-PrimarySMTPAddress {
     
     .PARAMETER VerifyInputFileForDuplicates
     By default input file is verified for duplicates
+	
+	.PARAMETER Operation
+	Operation which need to performed on maibox.
+	
+	Available operations
+	- AddProxyAddress 
+	- RemoveProxyAddress
+	- SetSMTPPrimaryAddress
     
     .PARAMETER Mode
-    The switch which define action to perform - default DisplayOnly
+    The switch which define action to perform - default mode is DisplayOnly
+	
+	Available modes
+	- DisplayOnly
+	- PerformActions
+	- CreatePerformActionsCommandsOnly
+	- Rollback
+	- CreateRollbackCommnadsOnly
 
-    .PARAMETER DomainName
+    .PARAMETER FQDNDomainName
     Active Directory domain name - FQDN
 
     .PARAMETER CreateRollbackFile
@@ -46,9 +61,10 @@ Function Set-PrimarySMTPAddress {
     KEYWORDS: PowerShell, Exchnange, Active Directory, SMTP
 
     VERSION HISTORY
-    0.1.0 - Initial release - untested !
-	0.2.0 - Tested, corrected
-	0.2.1 - Info about license added - GNU GPLv3
+    0.1.0 - 2015-03-12 - Initial release - untested !
+	0.2.0 - 2015-03-13 - Tested, corrected
+	0.2.1 - 2015-03-14 - Info about license added - GNU GPLv3
+	0.3.0 - 2015-03-15 - Mode set extended, Operation parameter added
 	
 	LICENSE
 	Copyright (C) 2015 Wojciech Sciesinski
@@ -65,14 +81,10 @@ Function Set-PrimarySMTPAddress {
     
          
     TO DO
-    The structure for input files need to be parsed.
 	Add checking if mailbox/recipient has enabled 'useemailaddresspolicy'
+	Skipping for objects need to be implemented
     
-    .EXAMPLE
-
-    Input CSV file format - <FILL>
-
-    
+   
     
 #> 
 
@@ -85,9 +97,13 @@ param (
     
     [parameter(Mandatory=$false)]
     [Bool]$VerifyInputFileForDuplicates=$true,
+	
+	[parameter(Mandatory=$true)]
+	[ValidateSet("AddProxyAddress","RemoveProxyAddress","SetSMTPPrimaryAddress")]
+	[String]$Operation,
 
     [parameter(Mandatory=$false)]
-    [ValidateSet("DisplayOnly","PerformActions","Rollback")]
+    [ValidateSet("DisplayOnly", "PerformActions", "CreatePerformActionsCommandsOnly", "Rollback", "CreateRollbackCommnadsOnly" )]
     [String]$Mode="DisplayOnly",
     
     [parameter(Mandatory=$false)]
@@ -199,11 +215,11 @@ PROCESS {
                         
                 $CurrentRecipientTest1 = $(Get-Recipient $_.RecipientIdentity -ErrorAction Stop | Where { $_.RecipientType -eq $RecipientType })
 
-		Write-Debug "First test for recipient result: $CurrentRecipientTest1"
+				Write-Debug "First test for recipient result: $CurrentRecipientTest1"
                
                 $CurrentRecipientTest2 = $(Get-Recipient $_.NewPrimarySMTPAddress -ErrorAction Stop | Where { $_.RecipientType -eq $RecipientType })
 
-		Write-Debug "Second test for recipient result: $CurrentRecipientTest2"
+				Write-Debug "Second test for recipient result: $CurrentRecipientTest2"
                
                 if ( $CurrentRecipientTest1.Guid -ne $CurrentRecipientTest2.Guid ) {
                 
@@ -222,7 +238,7 @@ PROCESS {
             
             Catch {
             
-                Write-Error "Recipient $_.RecipientIdentity or with address $_.NewPrimarySMTPAddress doesn't exist"
+                Write-Error "Recipient $($_).RecipientIdentity or with address $_.NewPrimarySMTPAddress doesn't exist"
                 
                 Break
             
@@ -433,236 +449,7 @@ END {
 }
 
 
-Function Test-EmailAddress {
-<#
-    .SYNOPSIS
-        Function is intended to verify the correctness of addresses email in Microsoft Exchange Enviroment
-        
-    .DESCRIPTION
-        Function which can be used to verifing an email address before for example adding it to Microosft Exchange environment. 
-        Checks perfomed: 
-        a) if email address contain wrong characters e.g. % or spaces
-        b) if email address is from domain which are on accepted domains list
-        c) if email address is currently assigned to any object in Exchange environment (a conflicted object exist)
-        As the result returned is PowerShell object which contain: EmailAddress, ExitCode, ExitDescription, ConflictedObjectAlias, ConflictedObjectType.
-        
-        Exit codes and descriptions:
-        0 - No Error
-        1 - Email doesn't contain 'at' char
-        2 - Email exist now
-        3 - Unsupported chars found
-        4 - Not accepted domain
-        5 - White chars e.g. spaces founded before/after email
-        
-    .PARAMETER EmailAddress
-        Email address which need to be verified in Exchange environment
 
-    .EXAMPLE
-        Test-EmailAddress -EmailAddress dummy@example.com
-    
-    .LINK
-        https://github.com/it-praktyk/Test-EmailAddress
-        
-    .LINK
-        https://www.linkedin.com/in/sciesinskiwojciech
-        
-    .NOTES
-        AUTHOR: Wojciech Sciesinski, wojciech[at]sciesinski[dot]net
-        KEYWORDS: Windows, PowerShell, Exchange Server, email
-        VERSION HISTORY
-        0.1.0 - 2015-02-13 - first draft
-        0.2.0 - 2015-02-16 - first working version
-        0.2.1 - 2-15-02-17 - minor updates, first version published on GitHub
-        0.3.0 - 2015-02-18 - exit codes added, result returned as PowerShell object
-        0.3.1 - 2015-02-18 - help updated, input parameater checks added
-        0.3.2 - 2015-02-19 - corrected for work with PowerShell 4.0 also (Windows Server 2012 R2)
-        0.3.3 - 2015-02-27 - ommited by mistake
-        0.3.4 - 2015-02-27 - regex for email parsing updated
-        0.3.5 - 2015-02-27 - chars like ' and # excluded from regex for parsing email address
-        0.4.0 - 2015-03-07 - verifying if function is runned in EMS added
-        0.5.0 - 2015-03-08 - verifying if email contains white chars (like a spaces) at the beginning or at the end added
-        0.5.1 - 2015-03-09 - corrected intentional error handling
-        
-
-        TODO
-        - add parameters to disable some checks
-        - add support for verifying emails from files directly 
-    
-
-        DISCLAIMER
-        This script is provided AS IS without warranty of any kind. I disclaim all implied warranties including, without limitation,
-        any implied warranties of merchantability or of fitness for a particular purpose. The entire risk arising out of the use or
-        performance of the sample scripts and documentation remains with you. In no event shall I be liable for any damages whatsoever
-        (including, without limitation, damages for loss of business profits, business interruption, loss of business information,
-        or other pecuniary loss) arising out of the use of or inability to use the script or documentation. 
-#>
-
-[cmdletbinding()]
-
-param (
-
-    [parameter(mandatory=$true)]
-    [ValidateNotNullOrEmpty()]
-    [alias("email")]
-    [String]$EmailAddress
-
-)
-
-BEGIN {
-
-    #Declare variable for store results data
-        
-    $Result = New-Object PSObject
-
-}
-
-PROCESS {
-
-    $AtPosition=$EmailAddress.IndexOf("@")
-
-    
-    If ( $AtPosition -eq -1 ) {
-    
-        Write-Verbose "Email address $EmailAddress is not correct - at char is missed."
-        
-        $Result | Add-Member -MemberType NoteProperty -Name EmailAddress -value $EmailAddress
-        $Result | Add-Member -MemberType NoteProperty -Name ExitCode  -value 1
-        $Result | Add-Member -MemberType NoteProperty -Name ExitDescription -value "Email doesn't contain 'at' char"
-        $Result | Add-Member -MemberType NoteProperty -Name ConflictedObjectAlias -value "Not checked"
-        $Result | Add-Member -MemberType NoteProperty -Name ConflictedObjectType -value "Not checked"
-                
-        Return $Result
-    }
-    Else { 
-    
-        #This try/catch block check if Exchange commands are available
-        Try {
-        
-            $AcceptedDomains = Get-AcceptedDomain 
-            
-        }
-        
-        Catch [System.Management.Automation.CommandNotFoundException] {
-        
-            Throw "This function need to be run using Exchange Management Shell."
-        
-        }
-    
-        Write-Verbose "Provided email address is $EmailAddress."
-        
-        If ( ($EmailAddress.Trim()).Length -ne $EmailAddress.Length ) {
-        
-            Write-Verbose -Message "Email address $EmailAddress contains white spaces at the beginning or at the end."
-                
-            $Result | Add-Member -MemberType NoteProperty -Name EmailAddress -value $EmailAddress
-            $Result | Add-Member -MemberType NoteProperty -Name ExitCode  -value 5
-            $Result | Add-Member -MemberType NoteProperty -Name ExitDescription -value "White chars e.g. spaces founded before/after email"
-            $Result | Add-Member -MemberType NoteProperty -Name ConflictedObjectAlias -value "Not checked"
-            $Result | Add-Member -MemberType NoteProperty -Name ConflictedObjectType -value  "Not checked"
-                
-            Return $Result
-        
-        }
-            
-        $EmailAddressLenght = $EmailAddress.Length
-
-        $Domain = $EmailAddress.Substring($AtPosition+1, $EmailAddressLenght - ( $AtPosition +1 ))
-        
-        Write-Verbose "Email address is from domain $Domain"    
-        
-        If ( ($AcceptedDomains | where { $_.domainname -eq $Domain } | measure).count -eq 1) {
-        
-            Write-verbose -Message "Domain from $EmailAddress found in accepted domains."
-            
-            $SpacePosition=$EmailAddress.IndexOf(" ")
-            
-            #Regex source http://www.regular-expressions.info/email.html
-            $EmailRegex = '[a-z0-9!#$%&''*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&''*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?'
-            
-            If ( ([regex]::Match($EmailAddress, $EmailRegex, "IgnoreCase ")).Success -and $SpacePosition -eq -1 ) {
-            
-                $NotError = $true
-            
-                Write-Verbose -Message "Email address  $EmailAddress  doesn't contain any unsupported chars"
-                
-                
-                Try {
-                    
-                    $Recipient = Get-Recipient $EmailAddress -ErrorAction Stop
-                    
-                }
-                                
-                Catch {
-                    
-                    Write-Verbose -Message "Email address doesn't exist in environment - finally result: is correct"
-                    
-                    $NotError = $false
-                    
-                    $Result | Add-Member -MemberType NoteProperty -Name EmailAddress -value $EmailAddress
-                    $Result | Add-Member -MemberType NoteProperty -Name ExitCode  -value 0
-                    $Result | Add-Member -MemberType NoteProperty -Name ExitDescription -value "No Error"
-                    $Result | Add-Member -MemberType NoteProperty -Name ConflictedObjectAlias -value "No conflict"
-                    $Result | Add-Member -MemberType NoteProperty -Name ConflictedObjectType -value "Not checked"
-                    
-                    Return $Result
-                
-                }
-    
-                If ( $NotError ) {
-                    
-                    Write-Verbose -Message "Recipient with email address $EmailAddress exist now."
-                        
-                    $Result | Add-Member -MemberType NoteProperty -Name EmailAddress -value $EmailAddress
-                    $Result | Add-Member -MemberType NoteProperty -Name ExitCode  -value 2
-                    $Result | Add-Member -MemberType NoteProperty -Name ExitDescription -value "Email exist now"
-                    $Result | Add-Member -MemberType NoteProperty -Name ConflictedObjectAlias -value $Recipient.alias
-                    $Result | Add-Member -MemberType NoteProperty -Name ConflictedObjectType -value $Recipient.RecipientType
-                
-                    Return $Result
-                        
-                }
-    
-            }
-            Else {
-        
-                Write-Verbose -Message "Email address $EmailAddress contain unsupported chars"
-                
-                $Result | Add-Member -MemberType NoteProperty -Name EmailAddress -value $EmailAddress
-                $Result | Add-Member -MemberType NoteProperty -Name ExitCode  -value 3
-                $Result | Add-Member -MemberType NoteProperty -Name ExitDescription -value "Unsupported chars found"
-                $Result | Add-Member -MemberType NoteProperty -Name ConflictedObjectAlias -value "Not checked"
-                $Result | Add-Member -MemberType NoteProperty -Name ConflictedObjectType -value  "Not checked"
-                
-                Return $Result
-        
-            }
-        }
-    
-        Else {
-        
-            Write-Verbose "Email address $EmailAddress is not from accepted domains."
-            
-            $Result | Add-Member -MemberType NoteProperty -Name EmailAddress -value $EmailAddress
-            $Result | Add-Member -MemberType NoteProperty -Name ExitCode  -value 4
-            $Result | Add-Member -MemberType NoteProperty -Name ExitDescription -value "Not accepted domain"
-            $Result | Add-Member -MemberType NoteProperty -Name ConflictedObjectAlias -value "Not checked"
-            $Result | Add-Member -MemberType NoteProperty -Name ConflictedObjectType -value  "Not checked"
-                    
-            Return $Result
-
-        }
-
-    }
-    
-}
-
-END {
-    
-    #Nothing yet in this section
-
-}
-
-}
 
 
 Function Start-NewTranscript {
@@ -780,7 +567,7 @@ PROCESS {
 
         Start-Transcript -Path $FullTranscriptFilePath -ErrorAction Stop
 
-        Write-Verbose "Transcript will be written to $FullTranscriptFilePath"
+        Write-Verbose -Message "Transcript will be written to $FullTranscriptFilePath"
     
 }
 
