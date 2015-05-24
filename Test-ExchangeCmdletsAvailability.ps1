55cc056c84d848b39d3f7c993ac1297db718d3c1
Wojciech Sciesinski <wojciech@sciesinski.net>
2015-05-25 00:17:50 +0200

    0.1.0 -  2015-05-25 - Initial release

---
+++ Test-ExchangeCmdletsAvailability.ps1
@@ -0,0 +1,93 @@
+﻿Function Test-ExchangeCmdletsAvailability {
+    
+<#
+    .SYNOPSIS
+    Function intended for veryfing if in current PowerShell session Exchange cmdlets and Exchange servers are available
+   
+    .DESCRIPTION
+    
+    .PARAMETER CmdletForCheck
+    Cmdlet which will be tested
+  
+    .EXAMPLE
+    
+    Test-ExchangeCmdletsAvailability -CmdletForCheck Get-Mailbox
+     
+    .LINK
+    https://github.com/it-praktyk/Test-ExchangeCmdletsAvailability
+    
+    .LINK
+    https://www.linkedin.com/in/sciesinskiwojciech
+          
+    .NOTES
+    AUTHOR: Wojciech Sciesinski, wojciech[at]sciesinski[dot]net
+    KEYWORDS: PowerShell,Exchange
+   
+    VERSIONS HISTORY
+    0.1.0 -  2015-05-25 - Initial release
+
+    TODO
+        
+    LICENSE
+    Copyright (C) 2015 Wojciech Sciesinski
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>
+   
+#>
+    
+    
+    [CmdletBinding()]
+    
+    param (
+        
+        [parameter(mandatory = $false)]
+        [String]$CmdletForCheck = "Get-ExchangeServer",
+        
+        [parameter(Mandatory = $false)]
+        [Bool]$CheckExchangeServersAvailability = $false
+                
+    )
+    
+    BEGIN {
+        
+        
+        
+        
+    }
+    
+    PROCESS {
+        
+        $FunctionAvailable = Test-Path -Path Function:$CmdletForCheck
+        
+        if ($FunctionAvailable -and $CheckExchangeServersAvailability) {
+            
+            $ReturnedServers = Get-ExchangeServer
+            
+            $ReturnedServerCount = ($ReturnedServers | measure).Count
+            
+            $Result = ($FunctionAvailable -and ($ReturnedServerCount -ge 1))
+            
+        }
+        Else {
+            
+            $Result = $FunctionAvailable
+            
+        }
+        
+    }
+    
+    END {
+        
+        Return $Result
+        
+    }
+    
+}
