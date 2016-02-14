366ae524eff04d93da7573331ebaec48980bd221
Wojciech Sciesinski <wojciech@sciesinski.net>
2016-02-14 22:18:09 +0100

    0.2.0 - 2016-02-10 - Report cappabilities removed from the function, input from pipeline added, the licence changed to MIT

--- Remove-DoubledSIPAddresses.ps1
+++ Remove-DoubledSIPAddresses.ps1
@@ -1,5 +1,4 @@
 ﻿function Remove-DoubledSIPAddresses {
-	
 <#
     .SYNOPSIS
     Function intended for verifying and removing doubled SIP addresses from all mailboxes in Exchange Server environment
@@ -8,18 +7,64 @@
     Function intended for verifying and removing doubled SIP addresses from all mailboxes in Exchange Server environment. `
     Only address in a domain provided in a parameter CorrectSIPDomain will be keep.
     
+    .PARAMETER Identity 
+    The Identity parameter specifies the identity of the mailbox. You can use one of the following values:
+    * GUID
+    * Distinguished name (DN)
+    * Display name
+    * Domain\Account
+    * User principal name (UPN)
+    * LegacyExchangeDN
+    * SmtpAddress
+    * Alias
+    
     .PARAMETER CorrectSIPDomain
-    Name of domain for which correct SIPs belong
+    Name of domain for which correct SIPs belong. If the parameter is not set domain from PrimarySMTPAddress will be used.
     
-    .PARAMETER CreateLogFile
-    By default log file is created
+    .INPUT
+    To see the input types that this cmdlet accepts, see Cmdlet Input and Output Types (http://go.microsoft.com/fwlink/p/?linkId=313798). If the Input Type field for a cmdlet is blank, the cmdlet doesn't accept input data.
+    The function accept the same input types like Get-Mailbox.
     
-    .PARAMETER LogFileDirectoryPath
-    By default log files are stored in the subfolder "logs" in current path, if the "logs" subfolder is missed will be created.
+    .OUTPUTS
+    PowerShell object what contains properties: MailboxAlias, MailboxDisplayName, PrimarySMTPAddress, MailboxGuid, SIPAddressesBeforeCount, SIPAddressesList, SIPAddresses
+    The SIPAddressesList property is a string with comma delimited list of SIPs. The SIPAddresses property is Microsoft.Exchange.Data.CustomProxyAddress data type and can be expanded.
+
+    .EXAMPLE
     
-    .PARAMETER LogFileNamePrefix
-    Prefix used for creating rollback/report files name. Default is "SIPs-Removed-"
-          
+    [PS] > Remove-DoubledSIPAddresses -Identity aa473815 -WhatIf -Verbose -CorrectSIPDomain contoso.com
+    
+    VERBOSE: Mailbox with alias AA473815 has assigned 2 SIP addresses.
+    What if: Performing operation "Remove SIP address sip:ingrid.thomes@example.com" on Target "mailbox: AA473815".
+
+    MailboxAlias              : AA473815
+    MailboxDisplayName        : Wolters-van der Thomes, IAV (Ingrid)
+    MailboxSMTPPrimaryAddress : ingrid.wolters-van.der.thomes@example.com
+    MailboxGuid               : b201434a-1f62-4ee4-a446-e0b2bc7badc9
+    SIPAddressesBeforeCount   : 2
+    SIPAddressesBeforeList    : SIP:Ingrid.Wolters@contoso.com,sip:ingrid.thomes@example.com
+    SIPAddressesBefore        : {SIP:Ingrid.Wolters@contoso.com,sip:ingrid.thomes@example.com}
+    SIPAddressAfterCount      : 2
+    SIPAddressesAfterList     : SIP:Ingrid.Wolters@contoso.com,sip:ingrid.thomes@example.com
+    SIPAddressesAfter         : {SIP:Ingrid.Wolters@contoso.com,sip:ingrid.thomes@example.com}
+    
+    .EXAMPLE
+    
+    [PS] > Remove-DoubledSIPAddresses -Identity aa473815 -Verbose
+    
+    VERBOSE: Mailbox with alias AA473815 has assigned 2 SIP addresses.
+	VERBOSE: SIP address Ingrid.Wolters@contoso.com is incorrect and will be deleted
+    
+    MailboxAlias              : AA473815
+    MailboxDisplayName        : Wolters-van der Thomes, IAV (Ingrid)
+    MailboxSMTPPrimaryAddress : ingrid.wolters-van.der.thomes@tailspintoys.com
+    MailboxGuid               : b201434a-1f62-4ee4-a446-e0b2bc7badc9
+    SIPAddressesBeforeCount   : 2
+    SIPAddressesBeforeList    : SIP:Ingrid.Wolters@contoso.com,sip:ingrid.thomes@tailspintoys.com
+    SIPAddressesBefore        : {SIP:Ingrid.Wolters@contoso.com,sip:ingrid.thomes@tailspintoys.com}
+    SIPAddressAfterCount      : 1
+    SIPAddressesAfterList     : SIP:ingrid.thomes@tailspintoys.com
+    SIPAddressesAfter         : {SIP:ingrid.thomes@tailspintoys.com}
+
     .LINK
     https://github.com/it-praktyk/Remove-DoubledSIPAddresses
     
@@ -33,127 +78,108 @@
     VERSIONS HISTORY
     0.1.0 - 2015-05-27 - First version published on GitHub
     0.1.2 - 2015-05-29 - Switch address to secondary befor remove, post-report corrected
-    0.1.3 - 2015-05-31 - Help updated, a script reformated
-	0.1.4 - 2015-06-09 - Primary SMTP address added to report file
-
-    TODO
-    - check if Exchange cmdlets are available
-    Function intended for verifying and removing doubled SIP addresses from all mailboxes in Exchange Server environment
+    0.1.3 - 2015-05-31 - Help updated, a script reformatted
+    0.1.4 - 2015-06-09 - Primary SMTP address added to report file
+    0.2.0 - 2016-02-10 - Report cappabilities removed from the function, input from pipeline added, the licence changed to MIT
         
     LICENSE
-    Copyright (C) 2015 Wojciech Sciesinski
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with this program. If not, see <http://www.gnu.org/licenses/>
+    Copyright (c) 2016 Wojciech Sciesinski
+    This function is licensed under The MIT License (MIT)
+    Full license text: https://opensource.org/licenses/MIT
    
-#>
+#>  
     
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess = $true)]
     Param (
         
-        [Parameter(Mandatory = $true, Position = 0)]
-        [String]$CorrectSIPDomain,
-        
-        [parameter(Mandatory = $false)]
-        [Bool]$CreateLogFile = $true,
-        
-        [parameter(Mandatory = $false)]
-        [String]$LogFileDirectoryPath = ".\logs\",
-        
-        [parameter(Mandatory = $false)]
-        [String]$LogFileNamePrefix = "SIPs-Removed-",
-        
-        [parameter(Mandatory = $false)]
-        [Bool]$DisplayProgressBar = $false
+        [parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, Position = 0)]
+        [Alias("Mailbox")]
+        $Identity,
+        [Parameter(Mandatory = $false, Position = 1)]
+        [String]$CorrectSIPDomain
         
     )
     
     
     BEGIN {
         
-        [String]$StartTime = Get-Date -format yyyyMMdd-HHmm
+        #Check if the function is run in the Exchange Management Shell
+        
+        [String]$CmdletForCheck = "Get-Mailbox"
+        
+        $CmdletAvailable = Test-Path -Path Function:$CmdletForCheck
+        
+        If (!$CmdletAvailable) {
+            
+            [String]$MessageText = "The function need to be run in the Exchange Management Shell"
+            
+            Throw $MessageText
+            
+        }
         
         $Results = @()
-        
-        [String]$MessageText = "Data about mailboxes are read from Active Directory - please wait"
-        
-        Write-Verbose -Message $MessageText
-        
-        $Mailboxes = Get-Mailbox -ResultSize Unlimited | Select -Property Alias, DisplayName, RecipientType, EmailAddresses, Guid
-        
-        $MailboxesCount = ($Mailboxes | measure).Count
-        
-        $i = 1
         
     }
     
     PROCESS {
         
-        $Mailboxes | ForEach  {
+        ForEach ($MailboxInLoop in $Identity) {
             
-            If ($DisplayProgressBar) {
+            If ($MailboxInLoop.gettype().fullname -eq 'Microsoft.Exchange.Data.Directory.Management.Mailbox') {
                 
-                $PercentCompleted = [math]::Round(($i / $MailboxesCount) * 100)
+                $CurrentMailbox = $MailboxInLoop | Select-Object -Property Alias, DisplayName, RecipientType, EmailAddresses, Guid, PrimarySMTPAddress
                 
-                $StatusText = "Percent completed $PercentCompleted%, currently the recipient {0} is checked. " -f $($_).ToString()
+            }
+            Else {
                 
-                Write-Progress -Activity "Checking SIP addresses" -Status $StatusText -PercentComplete $PercentCompleted
+                $CurrentMailbox = Get-Mailbox $MailboxInLoop | Select-Object -Property Alias, DisplayName, RecipientType, EmailAddresses, Guid, PrimarySMTPAddress
                 
             }
             
-            $CurrentMailbox = $_
+            $CurrentMailboxSIPAddressesBefore = ($CurrentMailbox | Select-Object -ExpandProperty EmailAddresses | Where-Object -FilterScript { $_.prefix -match 'SIP' })
             
-            [String]$MessageText = "Currently addresses for {0} are checked" -f $CurrentMailbox.DisplayName
+            $CurrentMailboxSIPAddressesBeforeCount = ($CurrentMailboxSIPAddressesBefore | Measure-Object).Count
             
-            Write-Verbose -Message $MessageText
-            
-            $CurrentMailboxSIPAddresses = ($CurrentMailbox | select -ExpandProperty EmailAddresses | where { $_.prefix -match 'SIP' })
-            
-            $CurrentMailboxSIPAddressesCount = ($CurrentMailboxSIPAddresses | Measure-Object).Count
-            
-            if ($CurrentMailboxSIPAddressesCount -gt 1) {
+            if ($CurrentMailboxSIPAddressesBeforeCount -gt 1) {
                 
-                $AddToLog = $false
+                if ([String]::IsNullOrEmpty($CorrectSIPDomain)) {
+                    
+                    $CorrectSIPDomain = $($CurrentMailbox.PrimarySMTPAddress).Domain
+                    
+                }
                 
-                [String]$MessageText = "Mailbox with identifier {0} resolved to {1} has assigned {2} SIP addresses." `
-                -f $CurrentMailboxIdentifier, $CurrentMailbox.DisplayName, $CurrentMailboxSIPAddressesCount
+                [String]$MessageText = "Mailbox with alias {0} has assigned {1} SIP addresses." -f $CurrentMailbox.Alias, $CurrentMailboxSIPAddressesBeforeCount
                 
                 Write-Verbose -Message $MessageText
                 
                 $Result = New-Object PSObject
                 
                 $Result | Add-Member -type 'NoteProperty' -name MailboxAlias -value $CurrentMailbox.Alias
-				
-				$Result | Add-Member -type 'NoteProperty' -name MailboxDisplayName -value $CurrentMailbox.DisplayName
-				
-				$Result | Add-Member -type 'NoteProperty' -Name MailboxSMTPPrimaryAddress -Value $CurrentMailbox.PrimarySMTPAddress
+                
+                $Result | Add-Member -type 'NoteProperty' -name MailboxDisplayName -value $CurrentMailbox.DisplayName
+                
+                $Result | Add-Member -type 'NoteProperty' -Name MailboxSMTPPrimaryAddress -Value $CurrentMailbox.PrimarySMTPAddress
                 
                 $Result | Add-Member -Type 'NoteProperty' -Name MailboxGuid -Value $CurrentMailbox.Guid
                 
-                $Result | Add-Member -Type 'NoteProperty' -Name SIPAddressesBeforeCount -Value $CurrentMailboxSIPAddressesCount
+                $Result | Add-Member -Type 'NoteProperty' -Name SIPAddressesBeforeCount -Value $CurrentMailboxSIPAddressesBeforeCount
                 
-                [String]$CurrentSIPAddressesList = [string]::Join(",", $($CurrentMailboxSIPAddresses | ForEach { $_.ProxyAddressString }))
+                [String]$CurrentSIPAddressesList = [string]::Join(",", $($CurrentMailboxSIPAddressesBefore | ForEach-Object -Process { $_.ProxyAddressString }))
                 
                 $Result | Add-Member -Type 'NoteProperty' -Name SIPAddressesBeforeList -Value $CurrentSIPAddressesList
                 
-                $CurrentMailboxSIPAddresses | foreach {
+                $Result | Add-Member -Type 'NoteProperty' -Name SIPAddressesBefore -Value $CurrentMailboxSIPAddressesBefore
+                
+                foreach ($CurrentSIPObject in $CurrentMailboxSIPAddressesBefore) {
                     
-                    $CurrentSIPObject = $_
-                    
-                    [String]$CurrentSIPAddressString = $_.AddressString
+                    [String]$CurrentSIPAddressString = $CurrentSIPObject.AddressString
                     
                     $AtPosition = $CurrentSIPAddressString.IndexOf("@")
                     
                     $SIPAddressLenght = $CurrentSIPAddressString.Length
                     
                     [String]$CurrentSIPDomain = $CurrentSIPAddressString.Substring($AtPosition + 1, $SIPAddressLenght - ($AtPosition + 1))
+                    
                     
                     If ($CurrentSIPDomain -ne $CorrectSIPDomain) {
                         
@@ -163,52 +189,52 @@
                             
                         }
                         
-                        $SIPToRemove = $CurrentSIPObject.ProxyAddressString
+                        [String]$SIPToRemove = $CurrentSIPObject.ProxyAddressString
                         
-                        [String]$MessageText = [String]$MessageText = "SIP address {0} is incorrect and will be deleted" `
-                        -f $CurrentSIPAddressString
+                        [String]$MessageText = [String]$MessageText = "SIP address {0} is incorrect and will be deleted." `
+                        -f $SIPToRemove
                         
-                        Write-Verbose -Message $MessageText
-                        
-                        Set-Mailbox -Identity $CurrentMailbox.Alias -EmailAddresses @{ remove = $SIPToRemove } -ErrorAction Continue
-                        
-                        $AddToLog = $true
-                        
-                        
+                        if ($PSCmdlet.ShouldProcess("mailbox: $($CurrentMailbox.Alias)", "Remove SIP address $SIPToRemove")) {
+                            
+                            Set-Mailbox -Identity $CurrentMailbox.Alias -EmailAddresses @{ remove = $SIPToRemove } -ErrorAction Continue
+                            
+                        }
+
                     }
                     
                 }
                 
-                $CurrentMailboxSIPAddressesAfter = (Get-Mailbox -Identity $CurrentMailbox.Alias | select -ExpandProperty EmailAddresses | where { $_.prefix -match 'SIP' })
+                $CurrentMailboxSIPAddressesAfter = (Get-Mailbox -Identity $CurrentMailbox.Alias | Select-Object -ExpandProperty EmailAddresses | Where-Object -FilterScript { $_.prefix -match 'SIP' })
                 
                 $CurrentMailboxSIPAddressesCountAfter = ($CurrentMailboxSIPAddressesAfter | Measure-Object).Count
                 
                 If ($CurrentMailboxSIPAddressesCountAfter -gt 1) {
                     
-                    [String]$CurrentSIPAddressesListAfter = [string]::Join(",", $($CurrentMailboxSIPAddressesAfter | ForEach {
-                        
-                        $_.ProxyAddressString
-                    }))
+                    [String]$CurrentSIPAddressesListAfter = [string]::Join(",", $($CurrentMailboxSIPAddressesAfter | ForEach-Object -Process { $_.ProxyAddressString }))
                     
                 }
                 
-                Else {
+                elseif ($CurrentMailboxSIPAddressesCountAfter -eq 1) {
                     
                     $CurrentSIPAddressesListAfter = $CurrentMailboxSIPAddressesAfter.ProxyAddressString
                     
                 }
                 
-                $Result | Add-Member -Type 'NoteProperty' -Name SIPAddressesAfterList -Value $CurrentSIPAddressesListAfter
-                
-                If ($AddToLog) {
+                Else {
                     
-                    $Results += $Result
+                    $CurrentMailboxSIPAddressesCountAfter = 0
                     
                 }
                 
+                $Result | Add-Member -Type 'NoteProperty' -Name SIPAddressesAfterCount -Value $CurrentMailboxSIPAddressesCountAfter
+                
+                $Result | Add-Member -Type 'NoteProperty' -Name SIPAddressesAfterList -Value $CurrentSIPAddressesListAfter
+                
+                $Result | Add-Member -Type 'NoteProperty' -Name SIPAddressesAfter -Value $CurrentMailboxSIPAddressesAfter
+                
+                $Results += $Result
+                
             }
-            
-            $i++
             
         }
         
@@ -217,48 +243,7 @@
     
     End {
         
-        If ($CreateLogFile) {
-            
-            #Check if rollback directory exist and try create if not
-            If (!$((Get-Item -Path $LogFileDirectoryPath -ErrorAction SilentlyContinue) -is [system.io.directoryinfo])) {
-                
-                New-Item -Path $LogFileDirectoryPath -Type Directory -ErrorAction Stop | Out-Null
-                
-            }
-            
-            $FullLogFilePath = $LogFileDirectoryPath + $LogFileNamePrefix + $StartTime + '.csv'
-            
-            Write-Verbose "Write rollback data to file $FullLogFilePath"
-            
-            #If export will not be unsuccessfull than display $Results to screen as the list - will be catched by Transcript
-            
-            Try {
-                
-                If (($Resulst | measure).Count -lt 1) {
-                    
-                    
-                    $Results | Export-CSV -Path $FullLogFilePath -NoTypeInformation -Delimiter ";" -Encoding UTF8 -ErrorAction SilentlyContinue
-                    
-                }
-                Else {
-                    
-                    $Result = New-Object PSObject
-                    
-                    $Result | Add-Member -type 'NoteProperty' -name Message -value "Nothing has not changed - no doubled SIPs found."
-                    
-                    $Results | Export-CSV -Path $FullLogFilePath -NoTypeInformation -Delimiter ";" -Encoding UTF8 -ErrorAction SilentlyContinue
-                    
-                }
-                
-            }
-            
-            Catch {
-                
-                Return $Result
-                
-            }
-            
-        }
+        Return $Results
         
     }
     
