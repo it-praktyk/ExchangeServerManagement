63a3b928397ca91b397bc420b1fa90965a742723
Wojciech Sciesinski <wojciech@sciesinski.net>
2016-06-26 03:06:26 +0200

    Demo outputs for versions 0.4.0 - 0.6.0 added

--- ConvertFrom-O365AddressesRSS/demo/demo-disk.ps1
+++ ConvertFrom-O365AddressesRSS/demo/demo-disk.ps1
@@ -1,6 +1,6 @@
-$version = "0.2.3"
+$version = "0.6.0"
 
-$xmlFileName = "O365IPAddressesChanges-20160621-233611.xml"
+$xmlFileName = "O365IPAddressesChanges-20160626-030036.xml"
 
 Import-Module .\ConvertFrom-O365AddressesRSS.ps1 -Force -ErrorAction Stop
 
@@ -10,13 +10,15 @@ If ( -not (Test-Path ".\demo\$version" -type container)) {
 
 }
 
+
+
+[String]$xmlFilePath = ".\demo\{0}\{1}" -f $version, $xmlFileName
+
 If (-not (Test-Path ".\demo\$version\$xmlFileName") -and (Test-Path -Path .\$xmlFileName) ) {
 
 	Move-Item -Path .\$xmlFileName -Destination ".\demo\$version\$xmlFileName"
 
 }
-
-[String]$xmlFilePath = ".\demo\{0}\{1}" -f $version, $xmlFileName
 
 $generalOutputFileName = "{0}-Output.csv" -f $xmlFileName.Replace('.xml', '')
 
@@ -26,6 +28,6 @@ $detailOutputFileName = "{0}-Output-Details.csv" -f $xmlFileName.Replace('.xml',
 
 [String]$detailOutputFilePath = ".\demo\{0}\{1}" -f $version, $detailOutputFileName
 
-ConvertFrom-O365AddressesRSS -Path $xmlFilePath | Export-Csv -Path $generalOutputFilePath -NoTypeInformation -Encoding UTF8
+ConvertFrom-O365AddressesRSS -Path $xmlFilePath | Export-Csv -Path $generalOutputFilePath -NoTypeInformation -Encoding UTF8 -delimiter ";"
 
-ConvertFrom-O365AddressesRSS -Path $xmlFilePath  | select-object -Property Guid,PublicationDate,DescriptionIsParsable,OperationType,QuickDescription -ExpandProperty SubChanges | Export-Csv -Path $detailOutputFilePath -NoTypeInformation -Encoding UTF8
+ConvertFrom-O365AddressesRSS -Path $xmlFilePath  | select-object -Property Guid,PublicationDate,DescriptionIsParsable,OperationType,QuickDescription -ExpandProperty SubChanges | Export-Csv -Path $detailOutputFilePath -NoTypeInformation -Encoding UTF8 -delimiter ";"
